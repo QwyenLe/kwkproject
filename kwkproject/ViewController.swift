9becadd175b0659c381ff829703a4cb01d32105e
qwyenle <cutegoldenpig@gmail.com>
2021-07-20 14:15:45 -0400

    committing completed layout

--- kwkproject/ViewController.swift
+++ kwkproject/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcome: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func selfcare(_ sender: UIButton) {
+    }
+    
+    @IBAction func jotjourn(_ sender: UIButton) {
+    }
 
+    @IBAction func mindfulmom(_ sender: UIButton) {
+    }
+    
 }
 
