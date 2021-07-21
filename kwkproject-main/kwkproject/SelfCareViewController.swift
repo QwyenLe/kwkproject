e3d526fba0ad6012b85d3b6631b4b9cff04b4142
dinacazun <87436886+dinacazun@users.noreply.github.com>
2021-07-21 13:45:50 -0400

    updated self care screen

---
+++ kwkproject-main/kwkproject/SelfCareViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SelfCareViewController.swift
+//  kwkproject
+//
+//  Created by Dina Cazun-Moreno on 7/21/21.
+//
+
+import UIKit
+
+class SelfCareViewController: UIViewController {
+    @IBOutlet var selfCareLabel: UILabel!
+    
+    @IBOutlet var drinkWaterText: UITextField!
+    @IBOutlet var drinkWaterDescription: UITextView!
+    
+    @IBOutlet var goOutsideText: UITextField!
+    @IBOutlet var goOutsideDescription: UITextView!
+    
+    @IBOutlet var treatYourselfText: UITextField!
+    @IBOutlet var treatYourselfDescription: UITextView!
+    
+    @IBOutlet var screenTimeText: UITextField!
+    @IBOutlet var screenTimeDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func homeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
